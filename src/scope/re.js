63fc52cf0d909367ee314893150dbdc5597de3df
Luis Zapata <luis.azapatazepeda@gmail.com>
2022-07-06 21:01:57 -0500

    Ejemplos de curso de Scope y Closure de Javascript

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+var firstName; // Undefined
+firstName = "Luis Zapata";
+console.log(firstName);
+
+var lastName = "Matías"; // declarar / asignar
+lastName = "Ana"; // reasignar
+console.log(lastName);
+
+var secondName = "David"; // declarando / asignando
+var secondName = "Ana"; // reasignando
+console.log(secondName);
+
+// Let
+
+let fruit = "Apple"; // declarar / asignar
+fruit = "Kiwi"; // reasignar
+console.log(fruit);
+
+// let fruit = "Banana";
+// console.log(fruit);
+
+// Const
+
+const animal = "dog"; // declara y asigna
+// animal = "cat"; // reasignando
+// const animal = "Snake";
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("Jetta");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+"use strict";
+PI = 3.1416;
+console.log(PI);
+
+function myFunction() {
+  "use strict";
+  return (PI = 3.1416);
+}
+
+console.log(myFunction());
